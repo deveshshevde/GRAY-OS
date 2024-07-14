3be68f62f690b3f10a891bb4bbf91b9ee9c49c1a
devesh shevde <deveshshevde@deveshs-MacBook-Air.local>
2024-07-14 21:11:25 +0530

    linker script remaining

---
+++ Makefile
@@ -0,0 +1,34 @@
+#assign the value of aarch64-linux-gnu to ARMGNU 
+#if not set 
+ARMGNU ?= aarch64-linux-gnu
+
+Compiler_ops = -Wall -nostdlib -nostartfiles -ffreestanding -Iinclude -mgeneral-regs-only
+#-mgeneral-regs-only dont know how this optimisation works will note down later!!
+ASMOPS = -Iinclude 
+
+BUILD_DIR = build
+SRC_DIR = src
+
+all : kernelV8.img
+
+clean :
+	rm -rf $(BUILD_DIR) *.img 
+
+$(BUILD_DIR)/%_c.o: $(SRC_DIR)/%.c
+	mkdir -p $(@D)
+	$(ARMGNU)-gcc $(Compiler_ops) -MMD -c $< -o $@
+
+$(BUILD_DIR)/%_s.o: $(SRC_DIR)/%.S
+	$(ARMGNU)-gcc $(ASMOPS) -MMD -c $< -o $@
+
+C_FILES = $(wildcard $(SRC_DIR)/*.c)
+ASM_FILES = $(wildcard $(SRC_DIR)/*.S)
+OBJ_FILES = $(C_FILES:$(SRC_DIR)/%.c=$(BUILD_DIR)/%_c.o)
+OBJ_FILES += $(ASM_FILES:$(SRC_DIR)/%.S=$(BUILD_DIR)/%_s.o)
+
+DEP_FILES = $(OBJ_FILES:%.o=%.d)
+-include $(DEP_FILES)
+
+kernelV8.img: $(SRC_DIR)/linker.ld $(OBJ_FILES)
+	$(ARMGNU)-ld -T $(SRC_DIR)/linker.ld -o $(BUILD_DIR)/kernelV8.elf  $(OBJ_FILES)
+	$(ARMGNU)-objcopy $(BUILD_DIR)/kernelV8.elf -O binary kernelV8.img
---
+++ src/BootUp.s
@@ -0,0 +1,31 @@
+.global start
+
+start:
+    mrs	x0, mpidr_el1
+    /*
+    The MPIDR_EL1 (Multiprocessor Affinity Register) is a system register 
+    in the ARM architecture that provides information about the identity 
+    and topology of processors in a multiprocessor system. This register 
+    is essential for distinguishing between different cores and managing 
+    processor-specific operations.
+    */		
+	and	x0, x0,#0xFF		
+	cbz	x0, main_core		
+    // Isolation the cor4e ID by masking last 8bit
+	b	halt
+halt:
+    b halt
+main_core:
+    adr x0, bss_start_addr
+    adr x1, bass_end_addr
+    sub x1, x1 , x0 ; will get total space of bss section in code since we have 
+                    ; to initialise it to '0' because they may be unini. global or
+                    ; static data
+    cbz x1, bss_mem_to_zero ; return if bss section is of 0 bytes
+bss_mem_to_zero_loop:
+    str xzr, [x0]   ; store 0 in the addr pointed by the x0(start of bss)
+    add x0, x0, #8  ; inc the pointer to make next byte 0
+    subs x1, x1, #8 ; sub 8 to find the remaining size 
+    b.gt bss_mem_to_zero_loop ; do again and again 
+bss_mem_to_zero:
+    ret 
